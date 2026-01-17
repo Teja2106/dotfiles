f07eb066bcbadc20b71ecc789b43909f05ef83ef
Teja2106 <teja182106@gmail.com>
2026-01-17 21:42:06 +0530

    added dot configs

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require("wezterm")
+
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+config.keys = {
+  -- Resize left
+  { key = "h", mods="ALT|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) },
+  { key = "h", mods="CTRL|ALT|SHIFT", action = act.AdjustPaneSize({ "Left", 1 }) },
+
+  -- Resize Right
+  { key = "l", mods="ALT|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) },
+  { key = "l", mods="CTRL|ALT|SHIFT", action = act.AdjustPaneSize({ "Right", 1 }) },
+
+  --Resize Up
+  { key = "k", mods="ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) },
+  { key = "k", mods="CTRL|ALT|SHIFT", action = act.AdjustPaneSize({ "Up", 1 }) },
+
+  -- Resize Down
+  { key = "j", mods="ALT|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) },
+  { key = "j", mods="CTRL|ALT|SHIFT", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+  -- Split Terminal
+  -- Horizontal Split
+  { key = "|", mods="CTRL|ALT|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+
+  -- Vertical
+  { key = "_", mods="CTRL|ALT|SHIFT", action = act.SplitVertical { domain = "CurrentPaneDomain" } }
+}
+config.use_fancy_tab_bar = true
+
+config.initial_cols = 172
+config.initial_rows = 48
+
+config.color_scheme = "AdventureTime"
+config.window_background_opacity = 0.87
+
+config.window_decorations = "RESIZE"
+config.font = wezterm.font("DepartureMono Nerd Font Mono")
+config.font_size = 13
+config.default_cursor_style = "SteadyUnderline"
+
+return config
